d37fa5f2876d8efeb4c220e7d66e40b168f9f76a
rozhkov <rozhko.artem@st.nkpk.op.edu.ua>
2026-05-02 17:38:55 +0300

    Добавлен Customer.java
    Signed-off-by: rozhkov <rozhko.artem@st.nkpk.op.edu.ua>

--- .idea/Customer.java
+++ .idea/Customer.java
@@ -10,33 +10,50 @@ public class Customer {
     private boolean orderSended;
     private boolean orderCanceled;
     private String orderCanceledReason;
+    private String orderCategory;
 
     public Customer(String orderName, String orderLocation, int orderDateSend,
                     int orderId, boolean orderRecieved, boolean orderSended,
                     boolean orderCanceled,
-                    double orderCost, String orderCanceledReason) throws Exception {
+                    double orderCost, String orderCanceledReason, String orderCategory)
+                    throws Exception {
     }
 
     public int getOrderId() {
         return orderId;
+        //возвращаем айди заказа
     }
 
-    public int getOrderDateSend() {
-        return orderDateSend;
+    public int void OrderDateSendMethod() {
+        Customer(orderDateSend);
+        //вызов метода тела
     }
 
     public String getOrderName() {
         return orderName;
+        //возвращаем "имя" заказа
     }
 
     public void setOrderStatus() {
         if (orderCanceled) {
+            //определаяемое статус заказа
             orderSended = true;
         }
     }
 
+    private void setOrderCategory(String orderCategory) {
+        this.orderCategory = orderCategory;
+    } // задаємо захищене значення категорії товару
+    //також схожа ситуація і з айди товару, воно може бути
+    //Визначальним значенням у пошуку товару на маркетплейсі у зв'язку з пошуком по айді товару
+
+
     protected void setOrderId(int orderId) {
         this.orderId = orderId;
+        //задаємо захищений значення айді замовлення
+        //так як зазвичай його використовують для пошуку товарів у маркетплейсі у випадках коли
+        //ім'я не відповідає дійсності (якщо посередник змінив його)
+        // Його можна використовувати для подання скарги або на повернення конкретного товару використовуючи
     }
 
 //    public void OrderCommisison() {} //метод OrderCommisison позволяет заказать сам заказ
